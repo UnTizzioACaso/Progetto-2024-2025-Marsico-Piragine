f01cadfb94d57bd3e6ad03050dcc1a87c1b576b7
simone-glitch <153381760+simone-glitch@users.noreply.github.com>
2025-11-20 16:42:37 +0100

    aggiunta implementazione della finestra del accountPopup.fxml

--- src/main/java/bankapp/progetto20242025piragine/controller/component/SidebarController.java
+++ src/main/java/bankapp/progetto20242025piragine/controller/component/SidebarController.java
@@ -1,4 +1,36 @@
 package bankapp.progetto20242025piragine.controller.component;
 
-public class SidebarController {
+import bankapp.progetto20242025piragine.controller.BranchController;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SidebarController extends BranchController {
+
+    @FXML
+    Button accountPopupButton;
+    @FXML
+    void openAccountPopup() //opening the account popup
+    {
+        try
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/bankapp/progetto20242025piragine/fxml/popup/accountPopup.fxml")); //getting the fxml in the loader
+            Parent root = loader.load(); //creating the node from the loader
+            Stage popupStage = new Stage(); //creating a new stage for the accountPopup
+            popupStage.setTitle("Account"); //setting the title
+            popupStage.initModality(Modality.APPLICATION_MODAL);
+            popupStage.setScene(new Scene(root));
+            popupStage.showAndWait();
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
 }
