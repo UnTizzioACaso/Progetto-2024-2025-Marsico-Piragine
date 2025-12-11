b3f77136a030f7658face7145d5bc8b665eb71ef
davide <davide.saullo@gmail.com>
2025-12-11 15:13:09 +0100

    finito register1 controller

--- src/main/java/bankapp/progetto20242025piragine/controller/page/Register1Controller.java
+++ src/main/java/bankapp/progetto20242025piragine/controller/page/Register1Controller.java
@@ -1,6 +1,7 @@
 package bankapp.progetto20242025piragine.controller.page;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
@@ -24,15 +25,19 @@ public class Register1Controller extends RegisterController {
     DatePicker birthDateRegisterDatePicker;
 
     @FXML
+    ChoiceBox<String> sexChoiceBox;
+
+    @FXML
     public void loadRegisterPage2() //loading the register page n.2
     {
-        if (surnameRegisterTextField.getText().isEmpty()  || nameRegisterTextField.getText().isEmpty() || birthPlaceRegisterTextField.getText().isEmpty() || birthDateRegisterDatePicker.getValue() == null) { errorMessageLabel.setText("Tutti i campi devono essere compilati!"); }
+        if (surnameRegisterTextField.getText().isEmpty()  || nameRegisterTextField.getText().isEmpty() || birthPlaceRegisterTextField.getText().isEmpty() || birthDateRegisterDatePicker.getValue()==null|| sexChoiceBox.getValue() == null) { errorMessageLabel.setText("Tutti i campi devono essere compilati!"); }
         else
         {
             user.setFirstName(nameRegisterTextField.getText());
             user.setLastName(surnameRegisterTextField.getText());
             user.setBirthPlace(birthPlaceRegisterTextField.getText());
             user.setBirthDate(birthDateRegisterDatePicker.getValue().toString());
+            user.setGender(sexChoiceBox.getValue());
             rootController.loadPage("/bankapp/progetto20242025piragine/fxml/page/register2.fxml", user); //loading next registerer page, is triggered by the enterRegisterButton's on action event
         }
 
--- src/main/java/bankapp/progetto20242025piragine/controller/page/Register2Controller.java
+++ src/main/java/bankapp/progetto20242025piragine/controller/page/Register2Controller.java
@@ -1,7 +1,6 @@
 package bankapp.progetto20242025piragine.controller.page;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 
@@ -9,30 +8,32 @@ import javafx.scene.control.Label;
 public class Register2Controller extends RegisterController {
 
     @FXML
-    TextField surnameRegisterTextField;
+    TextField streetRegisterTextField;
 
     @FXML
     Label errorMessageLabel;
 
     @FXML
-    TextField nameRegisterTextField;
+    TextField phoneNumberRegisterTextField1;
 
     @FXML
-    TextField birthPlaceRegisterTextField;
+    TextField stateRegisterTextField;
 
     @FXML
-    DatePicker birthDateRegisterDatePicker;
+    TextField provinceRegisterTextField;
+
+
 
     @FXML
     public void loadRegisterPage2() //loading the register page n.2
     {
-        if (surnameRegisterTextField.getText().isEmpty()  || nameRegisterTextField.getText().isEmpty() || birthPlaceRegisterTextField.getText().isEmpty() || birthDateRegisterDatePicker.getValue() == null) { errorMessageLabel.setText("Tutti i campi devono essere compilati!"); }
+        if (streetRegisterTextField.getText().isEmpty()  || phoneNumberRegisterTextField1.getText().isEmpty() || stateRegisterTextField.getText().isEmpty() || provinceRegisterTextField.getText()== null) { errorMessageLabel.setText("Tutti i campi devono essere compilati!"); }
         else
         {
-            user.setFirstName(nameRegisterTextField.getText());
-            user.setLastName(surnameRegisterTextField.getText());
-            user.setBirthPlace(birthPlaceRegisterTextField.getText());
-            user.setBirthDate(birthDateRegisterDatePicker.getValue().toString());
+            user.setFirstName(streetRegisterTextField.getText());
+            user.setLastName(phoneNumberRegisterTextField1.getText());
+            user.setBirthPlace(stateRegisterTextField.getText());
+            user.setBirthDate(provinceRegisterTextField.getText());
             rootController.loadPage("/bankapp/progetto20242025piragine/fxml/page/register2.fxml", user); //loading next registerer page, is triggered by the enterRegisterButton's on action event
         }
 
