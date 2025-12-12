516f9e870ec4ab18866d2a98caa400c43d600279
simone-glitch <153381760+simone-glitch@users.noreply.github.com>
2025-12-12 13:17:34 +0100

    Migliorata l’estetica di lastFiveExpenses e corretti alcuni bug in HomePage e QuickContact

--- src/main/java/bankapp/progetto20242025piragine/controller/component/SidebarController.java
+++ src/main/java/bankapp/progetto20242025piragine/controller/component/SidebarController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -14,22 +14,21 @@ import java.io.IOException;
 public class SidebarController extends BranchController {
 
     @FXML
-    Button accountPopupButton;
+    private ImageView accountPopupButton;
+
     @FXML
-    void openAccountPopup() //opening the account popup
-    {
-        try
-        {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/bankapp/progetto20242025piragine/fxml/popup/accountPopup.fxml")); //getting the fxml in the loader
-            Parent root = loader.load(); //creating the node from the loader
-            Stage popupStage = new Stage(); //creating a new stage for the accountPopup
-            popupStage.setTitle("Account"); //setting the title
+    private void openAccountPopup() {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/bankapp/progetto20242025piragine/fxml/popup/accountPopup.fxml")
+            );
+            Parent root = loader.load();
+            Stage popupStage = new Stage();
+            popupStage.setTitle("Account");
             popupStage.initModality(Modality.APPLICATION_MODAL);
             popupStage.setScene(new Scene(root));
             popupStage.showAndWait();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
