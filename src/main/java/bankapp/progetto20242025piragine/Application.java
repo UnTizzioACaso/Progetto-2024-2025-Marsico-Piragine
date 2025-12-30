da64aecfd93c7e64a14d443d4e8040cf740f5b66
davide <davide.saullo@gmail.com>
2025-12-30 18:15:18 +0100

    tema/chiaro scuro in parte

--- src/main/java/bankapp/progetto20242025piragine/Application.java
+++ src/main/java/bankapp/progetto20242025piragine/Application.java
@@ -8,20 +8,31 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
-    //running the application
-    @Override
-    public void start(Stage stage) throws IOException
-    {
-        DBInitializer.initialize(); //initialization
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("fxml/rootWindow.fxml")); //getting the main window's fxml
-        Scene scene = new Scene(fxmlLoader.load()); //crating the scene with the fxml
-        stage.setTitle("PSP bankkkkk!"); //setting the window's title
-        stage.setMinWidth(650); //setting the window's min width
-        stage.setMinHeight(650); //setting the window's min lenght
-        stage.setScene(scene); //setting the scene in the window
-        stage.show(); //showing the window
-    }
 
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        DBInitializer.initialize();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                Application.class.getResource("fxml/rootWindow.fxml")
+        );
+
+        Scene scene = new Scene(fxmlLoader.load());
+
+        // 🎨 CSS TEMA CHIARO
+        scene.getStylesheets().add(
+                Application.class
+                        .getResource("css/light.css")
+                        .toExternalForm()
+        );
+
+        stage.setTitle("PSP bankkkkk!");
+        stage.setMinWidth(650);
+        stage.setMinHeight(650);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
         launch();
