d88f5fed92c8f743db68b388affae1fd27bf2021
nick <piraginenicolaita@gmail.com>
2025-12-30 17:02:14 +0100

    aggiunta completa della pagina carte

--- src/main/java/bankapp/progetto20242025piragine/controller/component/CardController.java
+++ src/main/java/bankapp/progetto20242025piragine/controller/component/CardController.java
@@ -1,4 +1,23 @@
 package bankapp.progetto20242025piragine.controller.component;
 
-public class CardController {
+import bankapp.progetto20242025piragine.controller.BranchController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CardController extends BranchController
+{
+    @FXML
+    Button cardBlockButton, cardSettingsButton, showCardInformationsButton;
+
+    @Override
+    public void initializer()
+    {
+        cardBlockButton.setVisible(false);
+        cardBlockButton.setDisable(true);
+        cardSettingsButton.setVisible(false);
+        cardSettingsButton.setDisable(true);
+        showCardInformationsButton.setVisible(false);
+        showCardInformationsButton.setDisable(true);
+    }
+
 }
