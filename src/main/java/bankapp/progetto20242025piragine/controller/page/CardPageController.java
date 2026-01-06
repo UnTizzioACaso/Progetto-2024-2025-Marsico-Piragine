b041f5fb18773ccdf80575d12bd3177b63eced31
UnTizzioACaso <piraginenicolaita@gmail.com>
2026-01-06 19:38:38 +0100

    caricamento dell popup della creazione carte risolto

--- src/main/java/bankapp/progetto20242025piragine/controller/page/CardPageController.java
+++ src/main/java/bankapp/progetto20242025piragine/controller/page/CardPageController.java
@@ -2,12 +2,9 @@ package bankapp.progetto20242025piragine.controller.page;
 
 import bankapp.progetto20242025piragine.controller.BranchController;
 import bankapp.progetto20242025piragine.controller.component.CreditCardRectangleController;
-import bankapp.progetto20242025piragine.controller.popup.AccountPopupController;
 import bankapp.progetto20242025piragine.controller.popup.CreateCardPopupController;
-import bankapp.progetto20242025piragine.db.BankAccountDAO;
 import bankapp.progetto20242025piragine.db.Card;
 import bankapp.progetto20242025piragine.db.CardDAO;
-import bankapp.progetto20242025piragine.util.CardService;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -29,7 +26,7 @@ public class CardPageController extends BranchController
 
 
     @FXML
-    public void openCreateCard() throws SQLException
+    public void openCreateCard()
     {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/bankapp/progetto20242025piragine/fxml/popup/createCardPopup.fxml")); //getting the fxml in the loader
@@ -43,37 +40,36 @@ public class CardPageController extends BranchController
             popupStage.initModality(Modality.APPLICATION_MODAL); //blocking all application's windows except the popup
             popupStage.setScene(new Scene(root));
             popupStage.showAndWait(); //blocks openAccountPopup event until the app gets closed
+
         }
         catch (IOException e)
         {
             System.err.println("error loading the create card popup" + e.getMessage());
             e.printStackTrace();
         }
-
-        Card card= CardService.createCard(rootController.user.getUserID(), BankAccountDAO.getAccountByUserId(rootController.user.getUserID()).getIdAccount(), );
     }
 
     @Override
-    public void initializer() throws SQLException
-    {
-        List<Card> cards = CardDAO.getCardsByUserId(rootController.user.getUserID());
+    public void initializer() {
+        try {
+            List<Card> cards = CardDAO.getCardsByUserId(rootController.user.getUserID());
 
-        for (int i = 0; i < cards.size(); i++)
-        {
-            try
-            {
-                FXMLLoader cardRectangleLoader = new FXMLLoader(getClass().getResource("/bankapp/progetto20242025piragine/fxml/component/creditCardRectangle.fxml"));
-                Node cardRectangle = cardRectangleLoader.load();
-                CreditCardRectangleController controller = cardRectangleLoader.getController();
-                controller.setRootController(rootController);
-                controller.fill(cards.get(i));
-                cardsContainerVBox.getChildren().add(cardRectangle);
+            for (int i = 0; i < cards.size(); i++) {
+                try {
+                    FXMLLoader cardRectangleLoader = new FXMLLoader(getClass().getResource("/bankapp/progetto20242025piragine/fxml/component/creditCardRectangle.fxml"));
+                    Node cardRectangle = cardRectangleLoader.load();
+                    CreditCardRectangleController controller = cardRectangleLoader.getController();
+                    controller.setRootController(rootController);
+                    controller.fill(cards.get(i));
+                    cardsContainerVBox.getChildren().add(cardRectangle);
+                } catch (IOException e) {
+                    System.err.println("error loading the credit card rectangle" + e.getMessage());
+                    e.printStackTrace();
+                }
             }
-            catch (IOException e)
-            {
-                System.err.println("error loading the credit card rectangle" + e.getMessage());
-                e.printStackTrace();
-            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/bankapp/progetto20242025piragine/controller/page/HomePageController.java
+++ src/main/java/bankapp/progetto20242025piragine/controller/page/HomePageController.java
@@ -5,15 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
 import javafx.geometry.Insets;
-import javafx.geometry.VPos;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
-
-import java.io.IOException;
 
 public class HomePageController extends BranchController
 {
