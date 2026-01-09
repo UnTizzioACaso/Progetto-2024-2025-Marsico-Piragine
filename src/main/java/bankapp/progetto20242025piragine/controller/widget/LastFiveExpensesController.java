591508f51888bf66c7c5341be0f5ededf3cb23c4
UnTizzioACaso <piraginenicolaita@gmail.com>
2026-01-09 21:57:39 +0100

    aggiunto generazione pin durante il register, ma è spuntato un bug che non salva roba nel db

--- src/main/java/bankapp/progetto20242025piragine/controller/widget/LastFiveExpensesController.java
+++ src/main/java/bankapp/progetto20242025piragine/controller/widget/LastFiveExpensesController.java
@@ -13,29 +13,36 @@ import java.util.List;
 
 public class LastFiveExpensesController extends BranchController
 {
+    // VBox container that will hold the last five expense transactions
     @FXML
     private VBox lastFiveExpensesVBox;
 
-
-
-
+    // Initializes the widget and loads the last five expenses of the current user
     @Override
     public void initializer()
     {
-        for(int i = 0; i < 5; i++)
+        for(int i = 0; i < 5; i++) // Loop to load up to 5 transactions
         {
             try
             {
+                // Fetch all transactions sent by the current user
                 List<Transaction> transactions = TransactionDAO.getTransactionsBySender(rootController.user.getUserID());
+
+                // Load the FXML for a single transaction row
                 FXMLLoader transactionLoader = new FXMLLoader(getClass().getResource("LastFiveExpensesView.fxml"));
-                Node transaction = transactionLoader.load();
+                Node transaction = transactionLoader.load(); // Create the node from the FXML
+
+                // Get the controller for this transaction and set its values
                 TransactionController transactionController = transactionLoader.getController();
-                transactionController.SetCorrectValues(transactions.get(i));
+                transactionController.SetCorrectValues(transactions.get(i)); // Fill transaction data
+
+                // Add the transaction node to the VBox at position i+1
                 lastFiveExpensesVBox.getChildren().add(i+1, transaction);
             }
             catch (Exception e)
             {
-                System.err.println("error loading last five expense widget" + e.getMessage());
+                // Print error if loading any transaction fails
+                System.err.println("Error loading last five expense widget: " + e.getMessage());
                 e.printStackTrace();
             }
         }
