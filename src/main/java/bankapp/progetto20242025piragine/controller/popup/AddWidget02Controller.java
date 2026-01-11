36b9bfe942a0ae2a3360414d7dbba62d1f3b4bb5
UnTizzioACaso <piraginenicolaita@gmail.com>
2026-01-11 16:08:09 +0100

    metodo per oscurare i widget gia usati all interno del popup per aggiungere i widget

--- src/main/java/bankapp/progetto20242025piragine/controller/popup/AddWidget02Controller.java
+++ src/main/java/bankapp/progetto20242025piragine/controller/popup/AddWidget02Controller.java
@@ -1,6 +1,37 @@
 package bankapp.progetto20242025piragine.controller.popup;
 
 import bankapp.progetto20242025piragine.controller.BranchController;
+import bankapp.progetto20242025piragine.util.ThemeManager;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
-public class AddWidget02Controller extends BranchController {
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class AddWidget02Controller extends BranchController
+{
+
+
+    @FXML
+    private VBox widgetsVBox;
+
+    public void disableUsedWidgets(GridPane homePageGridPane)
+    {
+        ThemeManager.applyTheme(widgetsVBox.getScene(), rootController.user.getTheme());
+        Set<String> usedIds = homePageGridPane.getChildren().stream().map(Node::getId).filter(Objects::nonNull).collect(Collectors.toSet());
+
+        for (Node widget : widgetsVBox.getChildren())
+        {
+            if (usedIds.contains(widget.getId()))
+            {
+                widget.setDisable(true);
+            }
+        }
+    }
+
+
 }
--- src/main/java/bankapp/progetto20242025piragine/controller/popup/AddWidget11Controller.java
+++ src/main/java/bankapp/progetto20242025piragine/controller/popup/AddWidget11Controller.java
@@ -1,6 +1,10 @@
 package bankapp.progetto20242025piragine.controller.popup;
 
 import bankapp.progetto20242025piragine.controller.BranchController;
+import javafx.scene.layout.GridPane;
 
 public class AddWidget11Controller extends BranchController {
+    public void disableUsedWidgets(GridPane homePageGridPane) {
+
+    }
 }
