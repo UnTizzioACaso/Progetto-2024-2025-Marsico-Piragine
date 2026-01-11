f2bd2bf1e3c8c961e12796d047862bebe77513a1
UnTizzioACaso <piraginenicolaita@gmail.com>
2026-01-11 17:45:13 +0100

    testata aggiunta e rimozione del widget quick contact in posizione (0, 2)

--- src/main/java/bankapp/progetto20242025piragine/controller/popup/AddWidget02Controller.java
+++ src/main/java/bankapp/progetto20242025piragine/controller/popup/AddWidget02Controller.java
@@ -1,12 +1,15 @@
 package bankapp.progetto20242025piragine.controller.popup;
 
 import bankapp.progetto20242025piragine.controller.BranchController;
+import bankapp.progetto20242025piragine.controller.page.HomePageController;
 import bankapp.progetto20242025piragine.util.ThemeManager;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.util.Objects;
 import java.util.Set;
@@ -15,13 +18,13 @@ import java.util.stream.Collectors;
 public class AddWidget02Controller extends BranchController
 {
 
+    public HomePageController homePageController;
 
     @FXML
     private VBox widgetsVBox;
 
     public void disableUsedWidgets(GridPane homePageGridPane)
     {
-        ThemeManager.applyTheme(widgetsVBox.getScene(), rootController.user.getTheme());
         Set<String> usedIds = homePageGridPane.getChildren().stream().map(Node::getId).filter(Objects::nonNull).collect(Collectors.toSet());
 
         for (Node widget : widgetsVBox.getChildren())
@@ -29,8 +32,18 @@ public class AddWidget02Controller extends BranchController
             if (usedIds.contains(widget.getId()))
             {
                 widget.setDisable(true);
+                widget.setOpacity(0.5);
             }
         }
+        ThemeManager.applyTheme(widgetsVBox.getScene(), rootController.user.getTheme());
+    }
+
+    @FXML
+    public void addQuickContactWidget()
+    {
+        homePageController.addWidget("/bankapp/progetto20242025piragine/fxml/widget/quickContact.fxml", 0, 2);
+        Stage stage = (Stage) widgetsVBox.getScene().getWindow();
+        stage.close();
     }
 
 
