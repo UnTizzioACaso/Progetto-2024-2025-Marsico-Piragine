5e43138146da6371d404ab93063dcf73b8d0b70f
UnTizzioACaso <piraginenicolaita@gmail.com>
2026-01-14 19:49:23 +0100

    completati tutti i filtri per mandare una richiesta d'amicizia

--- src/main/java/bankapp/progetto20242025piragine/controller/popup/FriendshipRequestController.java
+++ src/main/java/bankapp/progetto20242025piragine/controller/popup/FriendshipRequestController.java
@@ -3,6 +3,7 @@ package bankapp.progetto20242025piragine.controller.popup;
 import bankapp.progetto20242025piragine.controller.BranchController;
 import bankapp.progetto20242025piragine.db.*;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 
@@ -19,6 +20,9 @@ public class FriendshipRequestController extends BranchController {
     private TextField searchByEmailField;
 
     @FXML
+    private Label errorLabel;
+
+    @FXML
     private ToggleButton toggleUsername;
 
     @FXML
@@ -28,14 +32,10 @@ public class FriendshipRequestController extends BranchController {
     private ToggleButton togglePhoneNumber;
 
 
-
     @FXML
     public void switchToUsername() {
-        searchByUsernameField.setDisable(false);
         searchByUsernameField.setVisible(true);
-        searchByPhoneNumberField.setDisable(true);
         searchByPhoneNumberField.setVisible(false);
-        searchByEmailField.setDisable(true);
         searchByEmailField.setVisible(false);
         toggleUsername.setStyle("-fx-background-color: red; -fx-text-fill: white; -fx-background-radius: 8 0 0 8; -fx-border-color: transparent;");
         toggleEmail.setStyle("-fx-background-color: white; -fx-text-fill: red; -fx-background-radius: 0; -fx-border-color: gray; -fx-border-width: 0 1 0 1;");
@@ -45,11 +45,8 @@ public class FriendshipRequestController extends BranchController {
 
     @FXML
     public void switchToPhoneNumber() {
-        searchByUsernameField.setDisable(true);
         searchByUsernameField.setVisible(false);
-        searchByPhoneNumberField.setDisable(false);
         searchByPhoneNumberField.setVisible(true);
-        searchByEmailField.setDisable(true);
         searchByEmailField.setVisible(false);
         toggleUsername.setStyle("-fx-background-color: white; -fx-text-fill: red; -fx-background-radius: 8 0 0 8; -fx-border-color: transparent;");
         toggleEmail.setStyle("-fx-background-color: white; -fx-text-fill: red; -fx-background-radius: 0; -fx-border-color: gray; -fx-border-width: 0 1 0 1;");
@@ -59,80 +56,89 @@ public class FriendshipRequestController extends BranchController {
 
     @FXML
     public void switchToEmail() {
-        searchByUsernameField.setDisable(true);
         searchByUsernameField.setVisible(false);
-        searchByPhoneNumberField.setDisable(true);
         searchByPhoneNumberField.setVisible(false);
-        searchByEmailField.setDisable(false);
         searchByEmailField.setVisible(true);
         toggleUsername.setStyle("-fx-background-color: white; -fx-text-fill: red; -fx-background-radius: 8 0 0 8; -fx-border-color: transparent;");
         toggleEmail.setStyle("-fx-background-color: red; -fx-text-fill: white; -fx-background-radius: 0; -fx-border-color: gray; -fx-border-width: 0 1 0 1;");
         togglePhoneNumber.setStyle("-fx-background-color: white; -fx-text-fill: red; -fx-background-radius: 0 8 8 0; -fx-border-color: transparent;");
     }
 
+    private void sendRequest(User user) //checking if requester has been blocked by requested
+    {
+
+        if (user != null)
+        {
+            errorLabel.setText("Utente non trovato");
+            return;
+        }
+        if(user.equals(rootController.user))
+        {
+            errorLabel.setText("Utente non trovato");
+            return;
+        }
+        boolean blocked = false;
+        try {blocked = BlockDAO.isBlocked(user.getUserID(), rootController.user.getUserID());}
+        catch (SQLException e)
+        {
+            System.err.println("error during checking block " + e.getMessage());
+            e.printStackTrace();
+        }
+        if(blocked)
+        {
+            errorLabel.setText("Utente non trovato");
+            return;
+        }
+        FriendRequest request = new FriendRequest(rootController.user.getUserID(), user.getUserID());
+        try {FriendRequestDAO.sendRequest(request);}
+        catch (SQLException e)
+        {
+            System.err.println("error during sending request " + e.getMessage());
+            e.printStackTrace();
+        }
+        errorLabel.setText("richiesta inviata");
+    }
+
     @FXML
-    public void sendRequest() {
+    public void findHypotheticalFriend() //trying to find if the user who will get the request exist
+    {
         //Send request with username
-        if (!searchByUsernameField.isDisable()) {
-            User user = new User();
-            try {
-                user = UserDAO.getUserByUsername(searchByUsernameField.getText());
-            } catch (SQLException e) {
+        if (searchByUsernameField.isVisible())
+        {
+            User user = null;
+            try {user = UserDAO.getUserByUsername(searchByUsernameField.getText());}
+            catch (SQLException e)
+            {
                 System.err.println("error during user research in db by username" + e.getMessage());
                 e.printStackTrace();
             }
-
-            if (user.getUsername() != null) {
-                FriendRequest request = new FriendRequest(rootController.user.getUserID(), user.getUserID());
-                try {
-                    FriendRequestDAO.sendRequest(request);
-                } catch (SQLException e) {
-                    System.err.println("error during request sendRequest " + e.getMessage());
-                    e.printStackTrace();
-                }
-            }
+            sendRequest(user);
         }
 
         //Send request with email
-        if (!searchByEmailField.isDisable()) {
+        else if (searchByEmailField.isVisible())
+        {
             User user = new User();
-            try {
-                user = UserDAO.getUserByEmail(searchByEmailField.getText());
-            } catch (SQLException e) {
+            try {user = UserDAO.getUserByEmail(searchByEmailField.getText());}
+            catch (SQLException e)
+            {
                 System.err.println("error during user research in db by email " + e.getMessage());
                 e.printStackTrace();
             }
-
-            if (user.getUsername() != null) {
-                FriendRequest request = new FriendRequest(rootController.user.getUserID(), user.getUserID());
-                try {
-                    FriendRequestDAO.sendRequest(request);
-                } catch (SQLException e) {
-                    System.err.println("error during request sendRequest " + e.getMessage());
-                    e.printStackTrace();
-                }
-            }
+            sendRequest(user);
         }
 
         //Send request with phone number
-        if (!searchByPhoneNumberField.isDisable()) {
+        else if (searchByPhoneNumberField.isVisible())
+        {
             User user = new User();
-            try {
-                user = UserDAO.getUserByCellphone(searchByPhoneNumberField.getText());
-            } catch (SQLException e) {
+            try {user = UserDAO.getUserByCellphone(searchByPhoneNumberField.getText());}
+            catch (SQLException e)
+            {
                 System.err.println("error during user research in db by phone number " + e.getMessage());
                 e.printStackTrace();
             }
-
-            if (user.getUsername() != null) {
-                FriendRequest request = new FriendRequest(rootController.user.getUserID(), user.getUserID());
-                try {
-                    FriendRequestDAO.sendRequest(request);
-                } catch (SQLException e) {
-                    System.err.println("error during request sendRequest " + e.getMessage());
-                    e.printStackTrace();
-                }
-            }
+            sendRequest(user);
         }
     }
 }
