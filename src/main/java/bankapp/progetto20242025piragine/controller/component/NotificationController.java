e20d86d691b98b9c721d4d4976f3dc6434ba145c
simone-glitch <153381760+simone-glitch@users.noreply.github.com>
2026-01-18 15:38:27 +0100

    NotificationController ed notification.fxml

---
+++ src/main/java/bankapp/progetto20242025piragine/controller/component/NotificationController.java
@@ -0,0 +1,15 @@
+package bankapp.progetto20242025piragine.controller.component;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Region;
+
+public class NotificationController {
+
+    @FXML
+    private Region transparentRegion;
+
+    public void readNotify() {
+
+    }
+
+}
