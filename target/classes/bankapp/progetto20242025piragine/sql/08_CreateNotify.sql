db6fbd31584bb6fd66bd03c1dfa741bdef481aa3
UnTizzioACaso <piraginenicolaita@gmail.com>
2026-01-19 14:14:07 +0100

    dovrei aver finito le notifiche

--- target/classes/bankapp/progetto20242025piragine/sql/08_CreateNotify.sql
+++ target/classes/bankapp/progetto20242025piragine/sql/08_CreateNotify.sql
@@ -3,12 +3,10 @@ CREATE TABLE IF NOT EXISTS Notify (
     user_id INTEGER,
     id_transaction INTEGER,
     id_friend_request INTEGER,
-    id_friend INTEGER,
     message TEXT,
     read BOOLEAN,
     data_creation DATETIME,
     FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE,
     FOREIGN KEY (id_transaction) REFERENCES Transaction1(id_transaction) ON DELETE CASCADE,
-    FOREIGN KEY (id_friend_request) REFERENCES Friendship(id_friendship) ON DELETE CASCADE,
-    FOREIGN KEY (id_friend) REFERENCES Friendship(id_friendship) ON DELETE CASCADE
+    FOREIGN KEY (id_friend_request) REFERENCES Friendship(id_friendship) ON DELETE CASCADE
 );
