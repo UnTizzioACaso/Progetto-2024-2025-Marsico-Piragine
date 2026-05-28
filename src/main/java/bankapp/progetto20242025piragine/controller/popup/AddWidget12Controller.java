314f069a4e4c597fab4d57594d6c8d3fe7cefb57
pirag <piraginenicolaita@gmail.com>
2026-05-28 13:56:27 +0200

    tutti gli add widget popup funzionano alla perferzione

--- src/main/java/bankapp/progetto20242025piragine/controller/popup/AddWidget12Controller.java
+++ src/main/java/bankapp/progetto20242025piragine/controller/popup/AddWidget12Controller.java
@@ -1,14 +1,18 @@
 package bankapp.progetto20242025piragine.controller.popup;
 
 import bankapp.progetto20242025piragine.controller.BranchController;
+import bankapp.progetto20242025piragine.dao.HomeWidgetCustomDAO;
+import bankapp.progetto20242025piragine.model.HomeWidgetCustom;
 import bankapp.progetto20242025piragine.util.CurrentSession;
 import bankapp.progetto20242025piragine.util.ThemeManager;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -16,7 +20,10 @@ import java.util.stream.Collectors;
 public class AddWidget12Controller extends BranchController
 {
     @FXML
-    private Node closeAddWidget12;
+    private Node closeAddWidget12, transactionHistoryGridPane, monthlyExpensesVBox, lastFiveExpensesVBox, monthlyIncomesVBox;
+
+    @FXML
+    private Region transactionHistoryRegion, monthlyExpensesRegion, lastFiveExpensesRegion, monthlyIncomesRegion;
 
     @FXML
     private VBox widgetsVBox;
@@ -27,14 +34,27 @@ public class AddWidget12Controller extends BranchController
     @Override
     public void initializer()
     {
-        Set<String> usedIds = CurrentSession.getHomePageController().homePageGridPane.getChildren().stream().map(Node::getId).filter(Objects::nonNull).collect(Collectors.toSet());
-
-        for (Node widget : widgetsVBox.getChildren())
+        List<HomeWidgetCustom> usedWidgets = HomeWidgetCustomDAO.getUsedWidgetsByUserId(CurrentSession.getLoggedUser().getUserID());
+        for(HomeWidgetCustom widget : usedWidgets)
         {
-            if (usedIds.contains(widget.getId()))
+            switch (widget.getTypeWidget())
             {
-                widget.setDisable(true);
-                widget.setOpacity(0.5);
+                case "transactionHistoryGridPane" -> {
+                    transactionHistoryRegion.setDisable(true);
+                    transactionHistoryGridPane.setOpacity(0.3);
+                }
+                case "monthlyExpensesVBox" -> {
+                    monthlyExpensesRegion.setDisable(true);
+                    monthlyExpensesVBox.setOpacity(0.3);
+                }
+                case "lastFiveExpensesVBox" -> {
+                    lastFiveExpensesRegion.setDisable(true);
+                    lastFiveExpensesVBox.setOpacity(0.3);
+                }
+                case "monthlyIncomesVBox" -> {
+                    monthlyIncomesRegion.setDisable(true);
+                    monthlyIncomesVBox.setOpacity(0.3);
+                }
             }
         }
     }
