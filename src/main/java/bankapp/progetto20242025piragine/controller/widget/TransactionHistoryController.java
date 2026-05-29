fe96188aa697d3ce211244f1e8c95cc7df5a2f15
pirag <piraginenicolaita@gmail.com>
2026-05-29 22:44:11 +0200

    sistemato date time per le transazioni nei widget

--- src/main/java/bankapp/progetto20242025piragine/controller/widget/TransactionHistoryController.java
+++ src/main/java/bankapp/progetto20242025piragine/controller/widget/TransactionHistoryController.java
@@ -7,9 +7,12 @@ import bankapp.progetto20242025piragine.util.CurrentSession;
 import bankapp.progetto20242025piragine.util.VisualTransactionCreator;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.DatePicker;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
+import java.awt.*;
+import java.sql.Timestamp;
 import java.util.List;
 
 public class TransactionHistoryController extends WidgetController
@@ -17,6 +20,11 @@ public class TransactionHistoryController extends WidgetController
     @FXML
     private GridPane transactionHistoryGridPane;
 
+    @FXML
+    private TextField transactionHistoryTextField;
+
+    @FXML
+    private DatePicker fromDatePicker, toDatePicker;
 
     @FXML
     private VBox transactionHistoryVBox;
@@ -40,6 +48,27 @@ public class TransactionHistoryController extends WidgetController
             transactionHistoryVBox.getChildren().add(visualTransaction);
         }
     }
+
+    @FXML
+    public void filter()
+    {
+        populate(transactionHistoryTextField.getText(), Timestamp.valueOf(fromDatePicker.getValue().atStartOfDay()), Timestamp.valueOf(toDatePicker.getValue().atStartOfDay()));
+    }
+
+    private void populate(String username, Timestamp from, Timestamp to)
+    {
+        List<Transaction> transactions = TransactionDAO.getFilteredTransactionsByAccount(BankAccountDAO.getIdAccountByUserId(CurrentSession.getLoggedUser().getUserID()), username, from, to);
+        for (Transaction transaction : transactions)
+        {
+            Node visualTransaction = VisualTransactionCreator.createVisualTransaction(CurrentSession.getRootController(), transaction);
+            transactionHistoryVBox.getChildren().add(visualTransaction);
+        }
+    }
+
+
+
+
+
 
 
 
