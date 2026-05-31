b6d8de100cfe619bb9517d2247287365d3dc7197
pirag <piraginenicolaita@gmail.com>
2026-05-31 23:34:04 +0200

    pin popup fxml creato

--- src/main/java/bankapp/progetto20242025piragine/controller/popup/PinPopupController.java
+++ src/main/java/bankapp/progetto20242025piragine/controller/popup/PinPopupController.java
@@ -1,6 +1,13 @@
 package bankapp.progetto20242025piragine.controller.popup;
 
 import bankapp.progetto20242025piragine.controller.BranchController;
+import javafx.fxml.FXML;
 
-public class PinPopupController extends BranchController {
+public class PinPopupController extends BranchController
+{
+    @FXML
+    public void checkPin()
+    {
+
+    }
 }
