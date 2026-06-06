157b819270aac1a686842eea36c8a6bc5b433a53
pirag <piraginenicolaita@gmail.com>
2026-06-06 19:08:39 +0200

    light css finito

--- src/main/java/bankapp/progetto20242025piragine/controller/component/TransactionController.java
+++ src/main/java/bankapp/progetto20242025piragine/controller/component/TransactionController.java
@@ -5,6 +5,7 @@ import bankapp.progetto20242025piragine.dao.BankAccountDAO;
 import bankapp.progetto20242025piragine.model.Transaction;
 import bankapp.progetto20242025piragine.dao.UserDAO;
 import bankapp.progetto20242025piragine.util.CurrentSession;
+import bankapp.progetto20242025piragine.util.ThemeManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Paint;
@@ -23,13 +24,14 @@ public class TransactionController extends BranchController
     {
         int id = 0;
         id = BankAccountDAO.getIdAccountByUserId(CurrentSession.getLoggedUser().getUserID());
-
+        ThemeManager.applyTheme(transactionSubjectLabel.getScene(), CurrentSession.getLoggedUser().getTheme());
         if (transaction.getBeneficiary() == id) //positive transactions
         {
             try
             {
                 String username = UserDAO.getUsernameByUserId(BankAccountDAO.getUserIdByAccountId(transaction.getSender()));
                 transactionValueLabel.setText("+" + transaction.getAmount().toString() + " €");
+                transactionValueLabel.setStyle("-fx-text-fill: green !important;");
                 transactionSubjectLabel.setText(username);
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
                 String cleanDate = transaction.getTransactionDate().toLocalDateTime().format(formatter);
@@ -47,7 +49,7 @@ public class TransactionController extends BranchController
             {
                 String username = UserDAO.getUsernameByUserId(BankAccountDAO.getUserIdByAccountId(transaction.getBeneficiary()));
                 transactionValueLabel.setText("-" + transaction.getAmount().toString() + " €");
-                transactionValueLabel.setTextFill(Paint.valueOf("red"));
+                transactionValueLabel.setStyle("-fx-text-fill: red !important;");
                 transactionSubjectLabel.setText(username);
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");;
                 String cleanDate = transaction.getTransactionDate().toLocalDateTime().format(formatter);
@@ -59,5 +61,6 @@ public class TransactionController extends BranchController
                 e.printStackTrace();
             }
         }
+
     }
 }
